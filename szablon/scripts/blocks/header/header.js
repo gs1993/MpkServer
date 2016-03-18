c72ab1f2c067474dbdbdac44fa23bba3e2c0ab68
Michal Sobiecki <michalsobiecki@yahoo.com>
2016-03-18 23:00:31 +0100

    Przygotowanie headera do podpinania paneli.

--- szablon/scripts/blocks/header/header.js
+++ szablon/scripts/blocks/header/header.js
@@ -5,10 +5,20 @@
 $(document).ready(function () {
   $('.header__corner').on("click", function () {
     $('.header__switcher').toggleClass('-hidden');
+    $('.headerProfile__switcher').addClass('-hidden');
   });
 
   $('.header__item').on("click", function () {
     $('.header__switcher').toggleClass('-hidden');
+  });
+
+  $('.headerProfile__corner').on("click", function () {
+    $('.headerProfile__switcher').toggleClass('-hidden');
+    $('.header__switcher').addClass('-hidden');
+  });
+
+  $('.headerProfile__item').on("click", function () {
+    $('.headerProfile__switcher').toggleClass('-hidden');
   });
 
   $('#panel-loginButton').on("click", function () {
