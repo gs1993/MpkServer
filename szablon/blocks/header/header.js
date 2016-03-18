8a769f684892bc2337fc2870a34799b3bcde0063
Michal Sobiecki <michalsobiecki@yahoo.com>
2016-03-19 00:20:35 +0100

    panelWelcome

--- szablon/blocks/header/header.js
+++ szablon/blocks/header/header.js
@@ -6,6 +6,7 @@ $(document).ready(function () {
   $('.header__corner').on("click", function () {
     $('.header__switcher').toggleClass('-hidden');
     $('.headerProfile__switcher').addClass('-hidden');
+    $('.panel').addClass('-hidden');
   });
 
   $('.header__item').on("click", function () {
@@ -15,6 +16,7 @@ $(document).ready(function () {
   $('.headerProfile__corner').on("click", function () {
     $('.headerProfile__switcher').toggleClass('-hidden');
     $('.header__switcher').addClass('-hidden');
+    $('.panel').addClass('-hidden');
   });
 
   $('.headerProfile__item').on("click", function () {
