b888cccc2c508003a83267500d9034438b15b987
Michal Sobiecki <michalsobiecki@yahoo.com>
2016-03-18 19:36:37 +0100

    Mapy Google Narazie JS, Header

--- szablon/scripts/blocks/header/header.js
+++ szablon/scripts/blocks/header/header.js
@@ -1,3 +1,13 @@
 /**
  * Created by rafal on 11.03.2016.
  */
+
+$(document).ready(function () {
+  $('.header__corner').on("mouseenter", function () {
+    $('.header__switcher').toggleClass('-hidden');
+  });
+
+  $('.header__switcher').on("click", function () {
+    $('.header__switcher').toggleClass('-hidden');
+  });
+});
