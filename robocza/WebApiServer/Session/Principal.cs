f337cf96f342e6aec1d5d906c756e72699781ce7
Raik <lukraik@gmail.com>
2016-03-19 19:45:18 +0100

    -- autoryzacja

---
+++ robocza/WebApiServer/Session/Principal.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using Data.Models;
+
+namespace WebApiServer.Session
+{
+    public class Principal: IPrincipal
+    {
+        private Identity _identity { get; set; }
+
+
+        public Principal(Identity identity)
+        {
+            _identity = identity;
+        }
+
+        public bool IsInRole(string role)
+        {
+            return _identity.UserRoles.Any(x => x.Name == role);
+        }
+
+        public IIdentity Identity => _identity;
+    }
+}
