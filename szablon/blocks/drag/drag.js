072bf13828c46eaf79c0d80acae31ce0f50d9209
Michal Sobiecki <michalsobiecki@yahoo.com>
2016-03-21 01:50:37 +0100

    Implementacja drag and drop

---
+++ szablon/blocks/drag/drag.js
@@ -0,0 +1,9 @@
+/**
+ * Created by salimike on 21.03.16.
+ */
+
+$(document).ready(function () {
+  $(function() {
+    $( "#dragger" ).draggable({ containment: "#containerDrag", scroll: false })
+  });
+});
