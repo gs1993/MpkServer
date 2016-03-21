8884ec3ab76fc0152c8ab57ee12f6acbea501688
Michal Sobiecki <michalsobiecki@yahoo.com>
2016-03-21 18:57:19 +0100

    Panele ruchome

--- szablon/blocks/drag/drag.js
+++ szablon/blocks/drag/drag.js
@@ -4,6 +4,6 @@
 
 $(document).ready(function () {
   $(function() {
-    $( ".dragger" ).draggable({ containment: "#containerDrag", scroll: false })
+    $( ".dragger" ).draggable({ cursor: "move", containment: "#containerDrag", scroll: false })
   });
 });
