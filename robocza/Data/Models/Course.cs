eb2e6806b1fbbd802a25d7ca4d3ca376069eb393
Raik <lukraik@gmail.com>
2016-03-27 23:47:12 +0200

    fix

---
+++ robocza/Data/Models/Course.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Models
+{
+    public class Course
+    {
+        public int Id { get; set; }
+
+        public bool Ended { get; set; }
+
+        public List<Activity> Activities { get; set; }
+
+        public Track Track { get; set; }
+
+        public Bus Bus { get; set; } 
+    }
+}
