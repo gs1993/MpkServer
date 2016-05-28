94e121d379696b883b3a42b7815ac5b14003fc05
rafal <rafal.kopryk@gmail.com>
2016-05-28 02:04:53 +0200

    wstepne markery autobusów

--- app/app.js
+++ app/app.js
@@ -1698,6 +1698,8 @@
       $scope.deleteMarkers();
       $scope.wayPoints = [];
       $scope.showBusstopMarkers();
+      $scope.showBusMarkers();
+
       $scope.map.directionsRenderers[0].setMap(null);
 
     };
@@ -1719,7 +1721,6 @@
     $scope.markerStart=[];
     $scope.markerTechnicla=[];
     $scope.markerTicket=[];
-    $scope.activityMarker = [];
 
     $scope.intCourse=function (kurs) {
       $scope.markerBusstopCheck=[];
@@ -1731,8 +1732,6 @@
       $scope.markerTechnicla=[];
       $scope.markerTicket=[];
 
-      $scope.activityMarker = [];
-      $scope.activityMarkers = [];
       angular.forEach(kurs.Activities, function (aktywnosc) {
         if (aktywnosc.ActivityType == 0) {
           aktywnosc.ActivityTypeName = "Sprawdzenie Biletu";
@@ -1800,7 +1799,6 @@
         }
 
       });
-      $scope.activityMarker=$scope.activityMarkers;
     };
 
 
@@ -1811,7 +1809,7 @@
     ///////////////////Autobusy////////////////////////////////
     $scope.busMarker = [];
     $scope.showBusMarkers = function () {
-      $scope.markerIcon = "../blocks/googleMaps/src/busMarker.png";
+
 
       $scope.busMarkers = [];
 
@@ -1823,25 +1821,21 @@
 
           if (kurs.Ended == false) {
 
-            $scope.busstopMarkers.push({
+            $scope.busMarkers.push({
               Id: kurs.Bus.Id,
               Name: kurs.Bus.BusNumber,
-              LocalizationString: autobus.LocalizationString,
-              GotMachineName: autobus.GotMachineName,
-              GotKioskName: autobus.GotKioskName,
-              BusStopTypeName: autobus.BusStopTypeName,
-              Position: [autobus.Lat, autobus.Lng]
+              Position: [kurs.Activities[kurs.Activities.length - 1].Lat, kurs.Activities[kurs.Activities.length - 1].Lng]
             });
+            console.log("test");
+            console.log($scope.busMarkers);
+            console.log("--------------");
           }
-
-
         });
       }).error(function (data, status, headers, config) {
         console.log("Błąd pobrania przystanków.")
       });
 
-      $scope.busstopMarker = $scope.busstopMarkers;
-
+      $scope.busMarker = $scope.busMarkers;
     };
 
 
